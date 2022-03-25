72259348a57968a40b2a3dc9f3607bf91f80e23d
suisrc <suisrc@outlook.com>
2022-03-25 14:40:41 +0000

    update mirror

--- vscode/mirror-npmjs.sh
+++ vscode/mirror-npmjs.sh
@@ -1,3 +1,6 @@
 # npmjs镜像
+#git config --global url."https://gitclone.com/github.com/".insteadOf https://github.com/
+#git config --global --add url."https://gitclone.com/github.com/".insteadOf git://github.com/
+#git config --global --add url."https://gitclone.com/github.com/".insteadOf ssh://git@github.com/
 npm config set registry https://registry.npm.taobao.org --global
 npm config set disturl https://npm.taobao.org/dist --global
