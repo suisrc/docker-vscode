3b672615fb95d770a03f4125bc46b2591a4bf2a0
suisrc <suisrc@outlook.com>
2026-07-14 01:33:46 +0800

    update code

--- srv/init.sh
+++ srv/init.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
 
+set -eu
 ## =======================================================================
 
 echo "======== init git config."
 # git config pull.rebase false
-if [ $GIT_USER_NAME ]; then
-    git config --global user.name "$GIT_USER_NAME"
+if [[ -n "${GIT_USER_NAME:-}" ]]; then
+    git config --global user.name "${GIT_USER_NAME}"
 fi
-if [ $GIT_USER_EMAIL ]; then
-    git config --global user.email "$GIT_USER_EMAIL"
+if [[ -n "${GIT_USER_EMAIL:-}" ]]; then
+    git config --global user.email "${GIT_USER_EMAIL}"
 fi
 
 ## =======================================================================
 
 if [ ! -f '/etc/ssh/_init' ]; then
-    echo `date -Iseconds` > /etc/ssh/_init
+    echo "$(date -Iseconds)" > /etc/ssh/_init
     echo '======== init sshd keys.'
-    mkdir /run/sshd
-    
+    mkdir -p /run/sshd
     # ssh-keygen 初始化
     echo y | ssh-keygen -q -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key
-    echo y | ssh-keygen -q -t rsa -N '' -f /etc/ssh/ssh_host_dsa_key
-    echo y | ssh-keygen -q -t rsa -N '' -f /etc/ssh/ssh_host_ecdsa_key
-    echo y | ssh-keygen -q -t rsa -N '' -f /etc/ssh/ssh_host_ed25519_key
+    echo y | ssh-keygen -q -t ecdsa -N '' -f /etc/ssh/ssh_host_ecdsa_key
+    echo y | ssh-keygen -q -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
     echo ''
     echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
-    if [[ -n "${USER}" ]] && [[ "$USER" != "root" ]]; then
+    if [[ -n "${USER:-}" ]] && [[ "$USER" != "root" ]]; then
         echo "======== init user: ${USER}."
         # 如果 USER 不是 root, 则创建用户
         if ! id -u "${USER}" >/dev/null 2>&1; then
-            mkdir -p /home/$USER && \
-            groupadd --gid 1000 $USER && \
-            useradd  --uid 1000 --gid $USER -d /home/$USER -m -s /usr/bin/zsh $USER && \
-            echo "${USER} ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/$USER && \
-            chmod 0440 /etc/sudoers.d/$USER && chmod g+rw /home && \
-            cat <<EOF > /home/$USER/.zshrc
+            mkdir -p /home/"${USER}"
+            groupadd --gid 1000 "${USER}"
+            useradd  --uid 1000 --gid "${USER}" -d /home/"${USER}" -m -s /usr/bin/zsh "${USER}"
+            echo "${USER} ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/"${USER}"
+            chmod 0440 /etc/sudoers.d/"${USER}" && chmod g+rw /home
+            cat <<EOF > /home/"${USER}"/.zshrc
 # .zshrc
 source /usr/share/powerlevel9k/powerlevel9k.zsh-theme
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-EOF && \
-            chown $USER:$USER /home/$USER/.zshrc
+EOF
+              chown "${USER}":"${USER}" /home/"${USER}"/.zshrc
         fi
         # 配置 ssh 的登录密码
         if [[ -n "${PASSWORD}" ]]; then
             echo "set ssh password for ${USER} ..."
-            echo "${USER}:${PASSWORD}" | chpasswd
+            chpasswd <<<"${USER}:${PASSWORD}"
         fi
     fi
     # 配置 root 用户的 ssh password
     if [[ -n "${PASSROOT}" ]]; then
         echo 'set ssh password for root ...'
-        echo "root:${PASSROOT}" | chpasswd
+        chpasswd <<<"root:${PASSROOT}"
         echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
     fi
 fi
